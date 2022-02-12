69cb76781ed1e82f03401e6b935b596c253b4cc0
Kamil <12999736+camaeel@users.noreply.github.com>
2022-02-12 22:23:21 +0100

    extract method

--- main.go
+++ main.go
@@ -4,30 +4,32 @@ import (
 	"context"
 	"encoding/json"
 	"log"
-	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func HandleRequest(ctx context.Context, event events.SQSEvent) (string, error) {
-	eventJson, err := json.MarshalIndent(event, "", "  ")
-	if err != nil {
-		log.Fatal("Can't log event")
-		return "", err
+	for i := range event.Records {
+		processRecord(event.Records[i])
 	}
-
-	log.Printf("EVENT: %s", eventJson)
-	// environment variables
-	log.Printf("REGION: %s", os.Getenv("AWS_REGION"))
-	// log.Println("ALL ENV VARS:")
-	// for _, element := range os.Environ() {
-	// 	log.Println(element)
-	// }
 
 	return "Ok", nil
 }
 
 func main() {
 	lambda.Start(HandleRequest)
+}
+
+func processRecord(record events.SQSMessage) error {
+	var result error
+
+	recordLog, err := json.Marshal(record)
+	if err != nil {
+		log.Fatal("Can't marshal event")
+		return err
+	}
+	log.Printf("Record: %s", recordLog)
+
+	return result
 }
