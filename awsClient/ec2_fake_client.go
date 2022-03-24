e6ef629961b3b6e1b8f660a1d631595d58555d15
Kamil <12999736+camaeel@users.noreply.github.com>
2022-03-24 22:59:53 +0100

    tests

--- awsClient/ec2_fake_client.go
+++ awsClient/ec2_fake_client.go
@@ -7,18 +7,21 @@ import (
 )
 
 type Ec2FakeClient struct {
-	DescribeTagsMock *ec2.DescribeTagsOutput
-	CreateTagsMock   *ec2.CreateTagsOutput
-	DescribeNIMock   *ec2.DescribeNetworkInterfacesOutput
+	DescribeTagsMock    *ec2.DescribeTagsOutput
+	DescribeTagsMockErr error
+	CreateTagsMock      *ec2.CreateTagsOutput
+	CreateTagsMockErr   error
+	DescribeNIMock      *ec2.DescribeNetworkInterfacesOutput
+	DescribeNIMockErr   error
 }
 
 func (c *Ec2FakeClient) DescribeTags(ctx context.Context, params *ec2.DescribeTagsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeTagsOutput, error) {
-	return c.DescribeTagsMock, nil
+	return c.DescribeTagsMock, c.DescribeTagsMockErr
 }
 
 func (c *Ec2FakeClient) CreateTags(ctx context.Context, params *ec2.CreateTagsInput, optFns ...func(*ec2.Options)) (*ec2.CreateTagsOutput, error) {
-	return c.CreateTagsMock, nil
+	return c.CreateTagsMock, c.CreateTagsMockErr
 }
 func (c *Ec2FakeClient) DescribeNetworkInterfaces(ctx context.Context, params *ec2.DescribeNetworkInterfacesInput, optFns ...func(*ec2.Options)) (*ec2.DescribeNetworkInterfacesOutput, error) {
-	return c.DescribeNIMock, nil
+	return c.DescribeNIMock, c.DescribeNIMockErr
 }
--- awsClient/ec2_test.go
+++ awsClient/ec2_test.go
@@ -56,3 +56,55 @@ func TestPrivateGetInstanceIpsErrorTest(t *testing.T) {
 	assert.Equalf(t, expectedPrivateIp, privateIp, "Wrong privateIp. Expected %s, got %s", expectedPrivateIp, privateIp)
 	assert.Equalf(t, expectedPublicIp, publicIp, "Wrong publicIp. Expected %s, got %s", expectedPublicIp, publicIp)
 }
+
+func TestPrivateGetInstanceIpsFromTagsTest(t *testing.T) {
+
+	expectedPrivateIp := "192.168.199.99"
+	expectedPublicIp := "99.100.101.102"
+	var expectedErr error = nil
+	instanceId := "i-fake123"
+
+	privateIpToken := "privateIp"
+	publicIpToken := "publicIp"
+
+	var fakeClient Ec2FakeClient = Ec2FakeClient{
+		DescribeTagsMock: &ec2.DescribeTagsOutput{
+			Tags: []types.TagDescription{
+				types.TagDescription{
+					Key:        &privateIpToken,
+					Value:      &expectedPrivateIp,
+					ResourceId: &instanceId,
+				},
+				types.TagDescription{
+					Key:        &publicIpToken,
+					Value:      &expectedPublicIp,
+					ResourceId: &instanceId,
+				},
+			},
+		},
+	}
+	ctx := context.TODO()
+
+	privateIp, publicIp, err := getInstanceIpsFromTags(ctx, &fakeClient, instanceId)
+	assert.Equalf(t, err, expectedErr, "Wrong err. Expected %s, got %s", expectedErr, err)
+	assert.Equalf(t, expectedPrivateIp, *privateIp, "Wrong privateIp. Expected %s, got %s", expectedPrivateIp, *privateIp)
+	assert.Equalf(t, expectedPublicIp, *publicIp, "Wrong publicIp. Expected %s, got %s", expectedPublicIp, *publicIp)
+}
+
+func TestPrivateGetInstanceIpsFromTagsErrorTest(t *testing.T) {
+
+	var expectedPrivateIp *string = nil
+	var expectedPublicIp *string = nil
+	var expectedErr error = fmt.Errorf("AWS dummy error")
+	instanceId := "i-fake123"
+
+	var fakeClient Ec2FakeClient = Ec2FakeClient{
+		DescribeTagsMockErr: expectedErr,
+	}
+	ctx := context.TODO()
+
+	privateIp, publicIp, err := getInstanceIpsFromTags(ctx, &fakeClient, instanceId)
+	assert.Equalf(t, err, expectedErr, "Wrong err. Expected %s, got %s", expectedErr, err)
+	assert.Equalf(t, expectedPrivateIp, privateIp, "Wrong privateIp. Expected %s, got %s", expectedPrivateIp, privateIp)
+	assert.Equalf(t, expectedPublicIp, publicIp, "Wrong publicIp. Expected %s, got %s", expectedPublicIp, publicIp)
+}
