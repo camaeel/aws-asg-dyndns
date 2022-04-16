73170f69fe1b4e07af9c6eaab8f33a7dd34232ef
Kamil <12999736+camaeel@users.noreply.github.com>
2022-04-16 22:24:23 +0200

    extract methods for start & terminate records

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -41,8 +42,6 @@ func main() {
 }
 
 func processRecord(ctx context.Context, ec2Client *ec2.Client, autoscalingClient *autoscaling.Client, ssmClient *ssm.Client, record events.SQSMessage) error {
-	var privateIp, publicIp *string
-
 	recordLog, err := json.Marshal(record)
 	if err != nil {
 		log.Fatal("Error! Can't marshal event")
@@ -60,72 +59,83 @@ func processRecord(ctx context.Context, ec2Client *ec2.Client, autoscalingClient
 	log.Printf("Record body: %s", recordBody)
 
 	if recordBody.LifecycleTransition == "autoscaling:EC2_INSTANCE_LAUNCHING" {
-		if recordBody.EC2InstanceId == "" {
-			return errors.New("body.EC2InstanceId not set")
-		}
-
-		privateIp, publicIp, err = awsClient.GetInstanceIps(ctx, ec2Client, recordBody.EC2InstanceId, false)
-		if err != nil {
-			log.Fatal("Error! Can't obtain IPs", err)
-			return err
-		}
-
-		log.Printf("Instance added: %s, got IPs: privateIp: %s, publicIp: %s", recordBody.EC2InstanceId, *privateIp, *publicIp)
-
-		err = awsClient.TagEC2Instance(ctx, ec2Client, recordBody.EC2InstanceId, privateIp, publicIp)
-		if err != nil {
-			log.Fatal("Error! Can't tag IPs on instance", err)
-			return err
-		}
-		log.Printf("Instance %s tagged with IPs", recordBody.EC2InstanceId)
-
-		if recordBody.NotificationMetadata != nil && recordBody.NotificationMetadata["domainList"] != nil {
-			for i := range recordBody.NotificationMetadata["domainList"] {
-				err := dns.DnsEntryAddIp(ctx, ssmClient, recordBody.NotificationMetadata["domainList"][i], publicIp)
-				if err != nil {
-					log.Printf("Error! Can't add DNS entry for ip: %s, domain: %s. %s", *publicIp, recordBody.NotificationMetadata["domainList"][i], err)
-					return err
-				}
-			}
-		}
-
-		err = awsClient.CompleteLifecycleHook(ctx, autoscalingClient, recordBody)
-		if err != nil {
-			log.Fatal("Error! Can't complete lifecyle hook. ", err)
-			return err
-		} else {
-			log.Printf("Lifecycle hook completed successful.")
-		}
-
+		return processInstanceLaunchRecord(ctx, ec2Client, autoscalingClient, ssmClient, recordBody)
 	} else if recordBody.LifecycleTransition == "autoscaling:EC2_INSTANCE_TERMINATING" {
-		privateIp, publicIp, err = awsClient.GetInstanceIps(ctx, ec2Client, recordBody.EC2InstanceId, true)
-		if err != nil {
-			log.Fatal("Error! Can't tag IPs on instance", err)
-			return err
-		}
+		return processInstanceTerminateRecord(ctx, ec2Client, autoscalingClient, ssmClient, recordBody)
+	}
 
-		log.Printf("Instance removed: %s, got IPs: privateIp: %s, publicIp: %s", recordBody.EC2InstanceId, *privateIp, *publicIp)
+	return fmt.Errorf("Error! Unknown LifecycleTransition: [%s]", recordBody.LifecycleTransition)
+}
 
-		if recordBody.NotificationMetadata != nil && recordBody.NotificationMetadata["domainList"] != nil {
-			for i := range recordBody.NotificationMetadata["domainList"] {
-				err := dns.DnsEntryRemoveIp(ctx, ssmClient, recordBody.NotificationMetadata["domainList"][i], publicIp)
-				if err != nil {
-					log.Printf("Error! Can't remove DNS entry for ip: %s, domain: %s. %s", *publicIp, recordBody.NotificationMetadata["domainList"][i], err)
-					return err
-				}
+func processInstanceLaunchRecord(ctx context.Context, ec2Client *ec2.Client, autoscalingClient *autoscaling.Client, ssmClient *ssm.Client, recordBody awsClient.LifecycleMessage) error {
+	var privateIp, publicIp *string
+
+	if recordBody.EC2InstanceId == "" {
+		return errors.New("body.EC2InstanceId not set")
+	}
+
+	privateIp, publicIp, err := awsClient.GetInstanceIps(ctx, ec2Client, recordBody.EC2InstanceId, false)
+	if err != nil {
+		log.Fatal("Error! Can't obtain IPs", err)
+		return err
+	}
+
+	log.Printf("Instance added: %s, got IPs: privateIp: %s, publicIp: %s", recordBody.EC2InstanceId, *privateIp, *publicIp)
+
+	err = awsClient.TagEC2Instance(ctx, ec2Client, recordBody.EC2InstanceId, privateIp, publicIp)
+	if err != nil {
+		log.Fatal("Error! Can't tag IPs on instance", err)
+		return err
+	}
+	log.Printf("Instance %s tagged with IPs", recordBody.EC2InstanceId)
+
+	if recordBody.NotificationMetadata != nil && recordBody.NotificationMetadata["domainList"] != nil {
+		for i := range recordBody.NotificationMetadata["domainList"] {
+			err := dns.DnsEntryAddIp(ctx, ssmClient, recordBody.NotificationMetadata["domainList"][i], publicIp)
+			if err != nil {
+				log.Printf("Error! Can't add DNS entry for ip: %s, domain: %s. %s", *publicIp, recordBody.NotificationMetadata["domainList"][i], err)
+				return err
 			}
 		}
+	}
 
-		err = awsClient.CompleteLifecycleHook(ctx, autoscalingClient, recordBody)
-		if err != nil {
-			log.Print("Error! Can't complete lifecyle hook. ", err)
-			return err
-		} else {
-			log.Printf("Lifecycle hook completed successful.")
-		}
+	err = awsClient.CompleteLifecycleHook(ctx, autoscalingClient, recordBody)
+	if err != nil {
+		log.Fatal("Error! Can't complete lifecyle hook. ", err)
+		return err
 	} else {
-		log.Printf("Warning! Unknown LifecycleTransition: [%s]", recordBody.LifecycleTransition)
-		return nil
+		log.Printf("Lifecycle hook completed successful.")
+	}
+
+	return nil
+}
+
+func processInstanceTerminateRecord(ctx context.Context, ec2Client *ec2.Client, autoscalingClient *autoscaling.Client, ssmClient *ssm.Client, recordBody awsClient.LifecycleMessage) error {
+	var privateIp, publicIp *string
+	privateIp, publicIp, err := awsClient.GetInstanceIps(ctx, ec2Client, recordBody.EC2InstanceId, true)
+	if err != nil {
+		log.Fatal("Error! Can't tag IPs on instance", err)
+		return err
+	}
+
+	log.Printf("Instance removed: %s, got IPs: privateIp: %s, publicIp: %s", recordBody.EC2InstanceId, *privateIp, *publicIp)
+
+	if recordBody.NotificationMetadata != nil && recordBody.NotificationMetadata["domainList"] != nil {
+		for i := range recordBody.NotificationMetadata["domainList"] {
+			err := dns.DnsEntryRemoveIp(ctx, ssmClient, recordBody.NotificationMetadata["domainList"][i], publicIp)
+			if err != nil {
+				log.Printf("Error! Can't remove DNS entry for ip: %s, domain: %s. %s", *publicIp, recordBody.NotificationMetadata["domainList"][i], err)
+				return err
+			}
+		}
+	}
+
+	err = awsClient.CompleteLifecycleHook(ctx, autoscalingClient, recordBody)
+	if err != nil {
+		log.Print("Error! Can't complete lifecyle hook. ", err)
+		return err
+	} else {
+		log.Printf("Lifecycle hook completed successful.")
 	}
 
 	return nil
